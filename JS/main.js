76ba1cfd3df9c01e8d2d46028be7c8bebc2c9732
Pavel Lukoyanov <148583866+complagaet@users.noreply.github.com>
2025-03-17 13:21:48 +0500

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     bobatron.scanner()
     setTimeout(() => {
         bobatron.scanner()
-    }, 200)
+    }, 1111)
 })
 
 window.addEventListener('resize', () => {
